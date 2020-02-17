f60a155858a1b033a1ec391664403aed6d7ebdad
marcmk6 <31750443+marcmk6@users.noreply.github.com>
2020-02-16 20:05:36 -0500

    fix - get corpus excerpt

--- Vanilla_SE8/src/corpus.py
+++ Vanilla_SE8/src/corpus.py
@@ -19,9 +19,10 @@ class Corpus:
                 content = row[2]
                 self.documents[doc_id] = (_Document(doc_id, title, content))
 
-    # FIXME
     def get_doc_excerpt(self, doc_id: str) -> str:
-        return self.documents[doc_id].content[:50]
+        """Return the first sentence as the excerpt"""
+        content = self.documents[doc_id].content
+        return content[:content.find('.') + 1]
 
     def get_doc_title(self, doc_id: str) -> str:
         return self.documents[doc_id].title
